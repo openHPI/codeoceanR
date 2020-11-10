450ce4c4bdb92c6f6b0a57dffb582ef41e32fff6
brry <berryboessenkool@hotmail.com>
2020-11-10 13:29:02 +0100

    rt_add_opened_files: is now vectorized

--- R/rt_add_opened_files.R
+++ R/rt_add_opened_files.R
@@ -1,20 +1,23 @@
 #' @title Add files to list of Rstudio opened source documents
 #' @description Add files to the list of Rstudio opened source documents.
 #' The related .Rproj Rstudio project should be closed at the time of writing.
-#' @return expanded filename, invisibly.
+#' @return expanded filenames, invisibly.
 #' @author Berry Boessenkool, \email{berry-b@@gmx.de}, Oct 2020
 #' @seealso [rt_create_task()], [exercise example](https://github.com/openHPI/codeoceanR/tree/main/inst/extdata) on github
 #' @keywords internal
 #'
-#' @param file Filename to be added, without path, must be present at `dir`.
+#' @param file Filename(s) to be added, without path, must be present at `dir`.
 #' @param dir  Target directory with a .Rproj file. DEFAULT: "." [getwd()]
 #' @param contextid Hexadecimal user ID, see [rt_get_context_id()]
 #'
-rt_add_opened_files <- function(file, dir=".", contextid=rt_get_context_id())
+rt_add_opened_files <- function(files, dir=".", contextid=rt_get_context_id())
 {
-# check file and dir:
-fullfile <- paste0(dir,"/",file)
-berryFunctions::checkFile(fullfile, pwd=FALSE)
+# check files and dir ----
+if(rt_is_OS("Linux")) files <- rev(files) # Not sure if this is needed on all Linux versions...
+#
+fullfiles <- paste0(dir,"/",files)
+names(fullfiles) <- files
+berryFunctions::checkFile(fullfiles, pwd=FALSE)
 berryFunctions::checkFile(dir)
 dir <- berryFunctions::normalizePathCP(dir)
 # check whether .Rproj file is present:
@@ -22,6 +25,10 @@ if(!any(grepl(".Rproj$", dir(dir)))) stop("No .Rproj file found at dir ", dir)
 # create hidden directory for list of Rstudio opened source documents:
 sfdir <- paste0(dir, "/.Rproj.user/",contextid,"/sources/per/t/")
 if(!dir.exists(sfdir)) dir.create(sfdir, recursive=TRUE)
+
+# actually add files ----
+for(file in files)
+{
 # generate (increasing) project-unique ID:
 id <- rt_nextlargestid(  dir(sfdir)[!grepl("contents",dir(sfdir))]   ) # function below
 # determine type:
@@ -32,7 +39,7 @@ type <- switch(tools::file_ext(file),
 # Write file metadata:
 cat('{
     "id": "',id,'",
-    "path": "',fullfile,'",
+    "path": "',fullfiles[file],'",
     "type": "',type,'",
     "project_path": "',file,'",
     "created": 0,
@@ -41,15 +48,18 @@ cat('{
 }
 ', file=paste0(sfdir, id), sep="")
 # copy file content:
-file.copy(from=fullfile, to=paste0(sfdir, id, "-contents"))
-# set active tab to first:
+file.copy(from=fullfiles[file], to=paste0(sfdir, id, "-contents"))
+}
+
+# set active tab to first ----
 panedir <- paste0(dir, "/.Rproj.user/",contextid,"/pcs/")
 if(!dir.exists(panedir)) dir.create(panedir)
 cat('{
     "activeTab": 0
 }', file=paste0(panedir, "source-pane.pper"), sep="")
+
 # output
-return(invisible(fullfile))
+return(invisible(fullfiles))
 }
 
 
