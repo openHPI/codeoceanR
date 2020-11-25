7c54562e199cab1ad2a53dd4b917e5c87d09a49e
brry <berryboessenkool@hotmail.com>
2020-11-26 00:07:03 +0100

    rt_due_warn: initial release

---
+++ R/rt_due_warn.R
@@ -0,0 +1,38 @@
+#' @title Warn about quiz submission time
+#' @description Warn quiz participant about due time.
+#' @return output of [rt_warn()]
+#' @author Berry Boessenkool, \email{berry-b@@gmx.de}, Nov 2020
+#' @seealso [rt_warn()]
+#' @export
+#' @examples
+#' rt_due_warn("2020-11-30 16:00")
+#' rt_due_warn("2020-11-30 16:00", begin=1e9, end=1e9)
+#'
+#' rt_due_warn(Sys.time()+125, format="%F %T") # format for sub-minute accuracy
+#' rt_due_warn(Sys.time()+30, format="%F %T")
+#'
+#' @param due   Charstring with due time in the format "2020-11-30 16:00".
+#'              Remember 15:00 UTC is 16:00 CET.
+#' @param tz    Timezone for [strptime]. DEFAULT: "CET"
+#' @param format Format for [strptime]. DEFAULT: "%Y-%m-%d %H:%M"
+#' @param begin Numeric: remaining time in minutes after which the message is displayed. DEFAULT: 7
+#' @param end   Numeric: remaining time in minutes after which the message is no longer displayed. DEFAULT: -15
+#' @param \dots Further arguments passed to [rt_warn()]
+#'
+rt_due_warn <- function(
+due,
+tz="CET",
+format="%Y-%m-%d %H:%M",
+begin=7,
+end=-15,
+...
+)
+{
+due <- strptime(due, format=format, tz=tz)
+remaining <- difftime(due, Sys.time(), units="mins")
+remaining <- as.numeric(round(remaining,1))
+task_id <- ""
+if(remaining<begin && remaining>end)
+rt_warn("Submission is due in ", remaining, " mins.",
+				if(remaining<1 && remaining>0) " --> Please submit now!", ...)
+}
