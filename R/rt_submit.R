2438c52ff0c2c52f74d68de6c9b40c5d5f50b206
brry <berryboessenkool@hotmail.com>
2020-11-28 09:50:44 +0100

    rt_submit: messages improved

--- R/rt_submit.R
+++ R/rt_submit.R
@@ -26,12 +26,13 @@ if(is.null(r)) return(NULL) # For non-interactive session
 
 # Add info to http errors (if any):
 # https://github.com/openHPI/codeocean/blob/master/app/controllers/remote_evaluation_controller.rb
+# https://httpstatuses.com/
 
 erm <- httr::http_condition(r, "error")$message
 if(!httr::status_code(r) %in% c(202, 207))
-	warning("Looks like something went wrong in rt_submit. Sorry! ",
-					"\nPlease open the CO task from within openHPI, click score, then submit.",
-					"\nThen send to following message to Berry, so it can be avoided next time.\n",
+	warning("Looks like something went wrong in the submission process. Sorry! ",
+					"\nPlease send to following message to Berry, so it can be avoided next time.",
+					"\n(Your grade will be added manually, so you really need to let him know.)\n",
 					toString(erm), call.=FALSE)
 httr::stop_for_status(r) # if any, pass http errors to R
 
@@ -39,7 +40,7 @@ httr::stop_for_status(r) # if any, pass http errors to R
 out <- httr::content(r, "parsed", "application/json")
 message(out$message, "\nThe submitted score is ", round(out$score,2), "%.",
 				"\nFeel free to continue the quiz including rt_score(), ",
-				"but don't submit again as that will decrease your grade.")
+				"but don't submit again.")
 
 # Output
 return(invisible(r))
