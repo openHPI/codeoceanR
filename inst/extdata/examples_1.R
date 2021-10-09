2d11f0794921dce0d69765bdee98e3c9ef7e6865
brry <berryboessenkool@hotmail.com>
2021-10-10 00:34:51 +0200

    example scripts renamed to enable rt_local_score

---
+++ inst/extdata/examples_1.R
@@ -0,0 +1,27 @@
+# Structure of task files
+
+# Task 0 -----
+# To make sure the testing suite is current, please run
+codeoceanR::rt_update_package() # see (Rstudio SHIFT + click) https://github.com/openHPI/codeoceanR#students
+
+
+# Task 1 -----
+# Create an object named 'my_first_object' with the number 99.
+
+
+
+# Make sure to save the script (CTRL + S) before running the following:
+codeoceanR::rt_score()
+# You can conveniently save + source (= run full script) in one go with  CTRL + SHIFT + S.
+
+
+
+# Task 2 -----
+# Create another object with the integer values from 5 till 15.
+# The desired objectnames is already included for your convenience.
+# Replace the zero with the indended code.
+my_second_object <- 0
+
+
+
+# Now continue in examples_2.R
--- inst/extdata/examples_tests.R
+++ inst/extdata/examples_tests.R
@@ -1,6 +1,8 @@
 # CodeOcean R exercises
 # Berry Boessenkool, berry-b@gmx.de, Jan 2020 - Oct 2021
 
+# This file should be hidden to students in real exercises.
+
 # This script is run by `rt_score()` (in Rstudio) or by clicking "Score" (in CodeOcean).
 # Running this script may never fail on CodeOcean, hence the usage of `try()` in rt_test_exercise.
 # The tests are set up so the rt_warn messages are increasingly specific.
@@ -10,15 +12,13 @@ library(codeoceanR) # for all functions prefixed with rt_
 
 rt_test_exercise({
 
-script1 <- rt_run_script("script_1.R") # script 1 ----
+script1 <- rt_run_script("examples_1.R") # script 1 ----
 
-rt_test_task(1, script=script1, object='my_first_object', class="numeric",
-             length=1, value=99)
-rt_test_task(2, script=script1, object='my_second_object', class="integer", length=11,
-             value=5:15)
+rt_test_task(1, script1, my_first_object , "numeric", length=1 , value=99)
+rt_test_task(2, script1, my_second_object, "integer", length=11, value=5:15)
 
 
-script2 <- rt_run_script("script_2.R") # script 1 ----
+script2 <- rt_run_script("examples_2.R") # script 1 ----
 
 sol <- rt_select_script_section(script2, 3)
 rt_test_task(3, script=sol,
