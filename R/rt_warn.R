5693b78648bac8c6ccf30de9671120ab2e5c2085
brry <berryboessenkool@hotmail.com>
2021-10-11 02:24:51 +0200

    rt_warn: invisible FALSE output for shorter conditional returns

--- R/rt_warn.R
+++ R/rt_warn.R
@@ -1,6 +1,6 @@
 #' @title warn about errors
 #' @description pass test messages to Rstudio / CodeOcean.
-#' @return Output of [warning()] in interactive mode (RStudio) or
+#' @return invisible FALSE and console printout of [message()] in interactive mode (RStudio) or
 #'                   [cat()] otherwise (CodeOcean)
 #' @author Berry Boessenkool, \email{berry-b@@gmx.de}, Oct 2020
 #' @keywords IO error
@@ -15,4 +15,5 @@ rt_warn <- function(...){
 	if(interactive())
               message("T", rt_env()$id, ": ", ...,       sep="") else
   cat("AssertionError: T", rt_env()$id, ": ", ..., "\n", sep="")
+  return(invisible(FALSE))
 }
