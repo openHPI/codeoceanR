befee6d17676d1cb2f6177a9e87f24bc91e6522e
brry <berryboessenkool@hotmail.com>
2021-10-19 20:44:47 +0200

    rt_contains: new argument 'ignore_quote', all spaces (now also tab,newline) ignored

--- R/rt_contains.R
+++ R/rt_contains.R
@@ -10,18 +10,21 @@
 #' @param value  value `obj` should have, can be char / numeric / other.
 #' @param fixed  Fixed match in [grepl()]? DEFAULT: TRUE
 #' @param ignore_space Remove spaces before comparison? DEFAULT: TRUE
+#' @param ignore_quote Replace `'` with `"` before comparison? DEFAULT: TRUE
 #'
-rt_contains <- function(obj, value, fixed=TRUE, ignore_space=TRUE){
+rt_contains <- function(obj, value, fixed=TRUE, ignore_space=TRUE, ignore_quote=TRUE){
   objname <- deparse(substitute(obj))
   if(!is.character(obj))
     {
     if(value %in% obj) return(TRUE)
     } else
     {
-    value2 <- if(ignore_space) gsub(" ", "", value) else value
-    obj2   <- if(ignore_space) gsub(" ", "", obj  ) else obj
+    value2 <- if(ignore_space) gsub("\\s", "", value) else value
+    obj2   <- if(ignore_space) gsub("\\s", "", obj  ) else obj
+    if(ignore_quote) value2 <- gsub("'", '"', value2)
+    if(ignore_quote) obj2   <- gsub("'", '"', obj2  )
     if(any(grepl(pattern=value2, x=obj2, fixed=fixed))) return(TRUE)
     }
-  rt_warn(objname, " does not contain ", toString(value))
+  rt_warn("'", objname, "' does not contain '", toString(value), "'")
   FALSE
 }
