da3fd6da416a9be0e7513aa5191d6a3cf0aad295
brry <berryboessenkool@hotmail.com>
2021-10-20 15:33:40 +0200

    rt_vec: new argument 'n' (and l+u)

--- R/rt_vec.R
+++ R/rt_vec.R
@@ -10,10 +10,14 @@
 #' message(rt_vec("hey"))
 #' message(rt_vec(letters[1:6]))
 #' message(rt_vec(sample(1:90, 7)))
+#' message(rt_vec(n=5))
 #' @param x input vector
+#' @param n number of integers to sample between `l` and `u`. DEFAULT: NULL
+#' @param l,u Lower and upper boundaries in sample, if n is given.
 #'
-rt_vec <- function(x)
+rt_vec <- function(x, n=NULL, l=-99, u=999)
 {
+if(!is.null(n)) x <- sample(l:u, n, replace=TRUE)
 ll <- length(x)!=1
 ch <- is.character(x)
 col <- if(ch) '","' else ','
